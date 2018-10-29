8371a4e7871e8688eb4d22c853bb10262f0f03d0
MChorin <miika.chorin@gmail.com>
2018-10-29 16:34:13 +0200

    Add project files.

---
+++ Miniprojekti2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Miniprojekti2.Startup))]
+namespace Miniprojekti2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
